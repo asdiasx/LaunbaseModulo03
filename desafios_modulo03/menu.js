0f1a4c7f8918a38189c2bd1bcd6567f0c73cfce8
Adriano Dias <adriano.rj.br@gmail.com>
2020-03-24 10:24:30 -0300

    Code  refactored to keep header and footer menu options on a separated file (menu.js).

---
+++ desafios_modulo03/menu.js
@@ -0,0 +1,50 @@
+module.exports = {
+    header: [{
+            url: "https://discordapp.com/invite/gCRAFhc",
+            target: "_blank",
+            label: "Comunidade"
+        },
+        {
+            url: "mailto:oi@rocketseat.com.br?subject=From Page Desafio1&body=Texto padrao de teste.",
+            target: "_blank",
+            label: "Email"
+        },
+        {
+            url: "tel:+5547992078767",
+            target: "_blank",
+            label: "Telefone"
+        },
+        {
+            url: "/",
+            target: "",
+            label: "Sobre"
+        }
+    ],
+    footer: [{
+            url: "/courses",
+            target: "",
+            label: "Conteúdos"
+        },
+
+        {
+            url: "https://github.com/asdiasx",
+            target: "_blank",
+            label: "Github"
+        },
+        {
+            url: "https://www.linkedin.com/in/adriano-d-6783a2/",
+            target: "_blank",
+            label: "Linkedin"
+        },
+        {
+            url: "https://twitter.com/Dragodian",
+            target: "_blank",
+            label: "Twitter"
+        },
+        {
+            url: "/",
+            target: "",
+            label: "Voltar"
+        }
+    ]
+};
